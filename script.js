6bf0e47efa67e4851dfad40e10ae40968091419c
Rohit Kumar Singh <155361024+ProjectsRO-hit@users.noreply.github.com>
2024-01-30 12:26:02 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const header = document.querySelector('header');
+const body = document.querySelector('main');
+const footer = document.querySelector('footer');
+
+header.style.minHeight = `${window.innerHeight * 0.1}px`;
+body.style.minHeight = `${window.innerHeight * 0.83}px`;
+footer.style.minHeight = `${window.innerHeight * 0.07}px`;
